c8938ddb6356843af6ebdf64aa77def68624bfbf
baptisteDixneuf <baptiste.dixneuf@gmail.com>
2014-05-24 18:51:25 +0200

    Création de compte et Validation

--- application/models/membership_model.php
+++ application/models/membership_model.php
@@ -1,7 +1,8 @@
 <?php
 class membership_model extends CI_Model 
 {
-	function validate(){
+	function validate()
+	{
 		$this->db->where('username',$this->input->post('username'));
 		$this->db->where('password',md5($this->input->post('password')));
 		$query = $this->db->get('membership');
@@ -11,6 +12,21 @@ class membership_model extends CI_Model
 			return true;
 		}
 
+	}
+
+	function create_member()
+	{
+		$new_member_insert_data= array(
+			'first_name' => $this->input->post('first_name') , 
+			'last_name' => $this->input->post('last_name') ,
+			'email_adress' => $this->input->post('email_adress') ,
+			'username' => $this->input->post('username') ,
+			'password' => md5($this->input->post('password'))
+
+		);
+
+		$insert=$this->db->insert('membership',$new_member_insert_data);
+		return $insert;
 	}
 }
 ?>
